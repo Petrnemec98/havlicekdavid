6b645a7d616f8bf309885bb35fd227bbf639fa99
Vojtěch Brzek <wnc@wnc.cz>
2023-03-30 12:26:11 +0200

    Turn off the debug mode

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -13,8 +13,7 @@ class Bootstrap
 	{
 		$configurator = new Configurator;
 		$appDir = dirname(__DIR__);
-		$configurator->setDebugMode(true);
-
+		//$configurator->setDebugMode(true);
 		//$configurator->setDebugMode('secret@23.75.345.200'); // enable for your remote IP
 		$configurator->enableTracy($appDir . '/log');
 
