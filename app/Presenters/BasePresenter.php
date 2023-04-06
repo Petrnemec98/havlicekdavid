a07efcd73aef9bd1d642d202163764437e2978bb
Vojtěch Brzek <wnc@wnc.cz>
2023-04-06 19:51:24 +0200

    Solved issue #6 - backlink behavior

--- app/Presenters/BasePresenter.php
+++ app/Presenters/BasePresenter.php
@@ -59,7 +59,58 @@ abstract class BasePresenter extends Nette\Application\UI\Presenter
 	public function beforeRender() {
 		$this->template->description = "";
 		$this->template->keywords = "";
-		$this->template->heading = "";
+		$this->template->backLink = $this->getBacklink();
+		$this->template->backLinkHistory = $this->getBacklinkHistory();
 	}
 
+	/**
+	 * Return relative part of URL as a href of the backlink button.
+	 * 
+	 * @return string
+	 */
+	protected function getBacklink() {
+		$httpRequest = $this->getHttpRequest();
+		$referer = $httpRequest->getReferer();
+		if ($referer === null) {
+			return $this->getDefaultBacklink();
+		}
+
+		if ($referer->host != $httpRequest->getUrl()->host) {
+			return $this->getDefaultBacklink();
+		}
+
+		return $referer->path.($referer->query?"?".$referer->query:"");
+	}
+
+	/**
+	 * Return true if we can use history.go(-1) function instead of link on the backlink button.
+	 * 
+	 * @return bool
+	 */
+	protected function getBacklinkHistory() {
+		$httpRequest = $this->getHttpRequest();
+		$referer = $httpRequest->getReferer();
+		if ($referer === null) {
+			return false;
+		}
+
+		if ($referer->host != $httpRequest->getUrl()->host) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Get URL of default BackLink (in case we can`t use referer or history.go function)
+	 * 
+	 * @return string
+	 * 
+	 */
+	private function getDefaultBacklink() {
+		if ($this->action == "default") {
+			return $this->link("Homepage:default");
+		} 
+		return $this->link(":default");
+	}
 }
